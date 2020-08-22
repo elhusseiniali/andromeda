25a4d9453110a76a1a67e7fbf2b7d886476cb14c
elhusseiniali <36609514+elhusseiniali@users.noreply.github.com>
2020-08-23 00:46:01 +0200

    Add basic test.

---
+++ andromeda/tests/test_general.py
@@ -0,0 +1,33 @@
+import os
+import unittest
+
+from andromeda import app, db
+
+
+TEST_DB = 'test.db'
+
+
+class BasicTests(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+            str(TEST_DB)
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
+
+        self.assertEqual(app.debug, False)
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
