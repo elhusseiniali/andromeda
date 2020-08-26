8fe3b0022f4ee4a2307dd9b583e906ca5819aacc
MisterMeri <mohammed.meri2@gmail.com>
2020-08-27 02:01:50 +0300

    Fixed user model test.

--- andromeda/tests/test_user_model.py
+++ andromeda/tests/test_user_model.py
@@ -4,20 +4,15 @@ from andromeda import User
 
 class UserModelTestCase(unittest.TestCase):
     def test_password_setter(self):
-        u = User(password='cat')
-        self.assertTrue(u.password_hash is not None)
-
-    def test_no_password_getter(self):
-        u = User(password='cat')
-        with self.assertRaises(AttributeError):
-            u.password
+        u = User(username="Test", email="test@test.com", password='cat')
+        self.assertTrue(u._password is not None)
 
     def test_password_verification(self):
-        u = User(password='cat')
+        u = User(username="Test", email="test@test.com", password='cat')
         self.assertTrue(u.verify_password('cat'))
         self.assertFalse(u.verify_password('dog'))
 
     def test_password_salts_are_random(self):
-        u = User(password='cat')
-        u2 = User(password='cat')
-        self.assertTrue(u.password_hash != u2.password_hash)
+        u = User(username="Test", email="test@test.com", password='cat')
+        u2 = User(username="Test2", email="test2@test.com", password='cat')
+        self.assertTrue(u._password != u2._password)
