6d2210e6b34f1aac42c0cb8e2177216a64f05a5d
elhusseiniali <36609514+elhusseiniali@users.noreply.github.com>
2020-08-29 12:45:40 +0200

    Reduce file age limit. Remember to change it back in production.

--- andromeda/__init__.py
+++ andromeda/__init__.py
@@ -9,6 +9,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '60808326457a6384f78964761aaa161c'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
 db = SQLAlchemy(app)
