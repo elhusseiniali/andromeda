4c4a5ad5ae8c3f7e6b4d28f4e923542dff2edb7e
elhusseiniali <36609514+elhusseiniali@users.noreply.github.com>
2020-09-05 21:01:15 +0200

    Add argument names when creating objects to make tests more readable.

--- andromeda/tests/test_models.py
+++ andromeda/tests/test_models.py
@@ -21,35 +21,47 @@ class TestObjectCreation(unittest.TestCase):
         assert paris
         assert berlin
 
-        mrh26 = User("mrh26", "mariahajj5@gmail.com", "1234", "+96170401523")
+        mrh26 = User(username="mrh26",
+                     email="justatest@gmail.com",
+                     password="1234",
+                     phone_number="+96170405060")
 
         assert mrh26
 
-        andromeda = Company("Andromeda",
-                            "mariahajj5@gmail.com",
-                            "+96170401523",
-                            10)
+        andromeda = Company(name="Andromeda",
+                            email="company@gmail.com",
+                            phone_number="+96170404040",
+                            ticket_quota=10)
 
         assert andromeda
 
-        employment1 = Employment(mrh26, andromeda)
+        employment1 = Employment(user=mrh26, company=andromeda)
 
         assert employment1
 
-        passport1 = Passport("Maria", "Hajj",
-                             "1998-02-12", france,
-                             "2020-08-20", "2021-08-20")
+        passport1 = Passport(user=mrh26,
+                             first_name="Bratan",
+                             last_name="The Tester",
+                             date_of_birth="1998-02-12",
+                             country=france,
+                             issue_date="2020-08-20",
+                             expiration_date="2021-08-20")
 
         assert passport1
 
-        flight1 = Flight("MEA200", berlin, paris,
-                         "2020-09-04 23:30:00", "2020-09-05 04:30:00")
+        flight1 = Flight(name="MEA200",
+                         departure_city=berlin,
+                         arrival_city=paris,
+                         departure="2020-09-04 23:30:00",
+                         arrival="2020-09-05 04:30:00")
 
         assert flight1
 
-        booking = Booking(flight1, mrh26, employment1,
-                          "2020-09-01", date_issued=datetime.now(),
+        booking = Booking(flight=flight1,
+                          user=mrh26,
+                          issuing_employment=employment1,
+                          date_issued=datetime.now(),
+                          cancellation_deadline="2020-09-01",
                           cancellation_fee=0)
 
         assert booking
-
